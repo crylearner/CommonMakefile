c26679d34024d18f619e58317772b2a029490798
yaoyufei <yaoyufei@yfyao.com>
2013-12-21 21:09:48 +0800

    添加编译静态库的通用规则static-library.mk

--- build/platforms/platform.mk
+++ build/platforms/platform.mk
@@ -6,6 +6,8 @@
 ## 定义编译命
 CXX:=g++
 CXXFLAGS:=-Wall
+AR:=ar
+ARFLAGS:=-rv
 
 ## 加载额外定制的编译命令配置.mk
 include build/platforms/platform-specs.mk
---
+++ build/targets/static-library.mk
@@ -0,0 +1,18 @@
+# static-library.mk
+#
+# Created on: 2013-12-21
+#     Author: lenovo
+
+## 编译生成obj文件的通用规则
+include build/targets/object.mk
+	
+## 定义可执行文件hello.exe的编译规则	
+$(TARGET):	$(OBJS)
+	$(AR) $(ARFLAGS) $@ $^
+
+
+all:	$(TARGET)
+
+## 定义 目标clean，一般就是删除所有obj文件，以及可执行文件。
+clean:
+	$(RM) $(TARGET) $(OBJS) $(DEPS)
