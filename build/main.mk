349be7a0788444e60500487093601a231513b415
yaoyufei <yaoyufei@yfyao.com>
2013-12-21 23:34:38 +0800

    引入module.mk

--- build/main.mk
+++ build/main.mk
@@ -8,23 +8,6 @@
 .PHONY : all clean
 all:
 
-include build/systems/system.mk
-
-## 定义产品信息。这里BUILD_SPECS设置为spec.mk
-PRODUCT_SPECS :=build/spec.mk
-include build/products/product.mk
-
-## 定义平台相关的编译命令。这里PLATFORM_SPECS为空，表示我们不做额外定制化。
-PLATFORM_SPECS :=
-include build/platforms/platform.mk
-
-## 将产品信息中的定义的feature与平台定义的编译选项混合
-CXXFLAGS+=$(OPTIONS)
-
-## 只要给定源文件目录以及目标hello.exe
-## 调用executable.mk，就可以自动编译得到想要的可执行文件
-SRC_FILES:= hello.cpp main.cpp
-TARGET:=hello.exe
-
-## 定义了如何生成可执行文件的通用规则
-include build/targets/executable.mk
+## 调用哪个模块mk,就编译哪个模块。
+## 也可以同时将多个模块导入
+include module.mk
