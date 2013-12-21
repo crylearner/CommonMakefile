23823b74c5a18bda16070899cf843eb43fbd2bb2
yaoyufei <yaoyufei@yfyao.com>
2013-12-21 20:29:54 +0800

    自动生成头文件依赖关系

--- build/main.mk
+++ build/main.mk
@@ -16,14 +16,19 @@ include build/platforms/platform.mk
 ## 将产品信息中的定义的feature与平台定义的编译选项混合
 CXXFLAGS+=$(OPTIONS)
 
+
+SRC_FILES:= hello.cpp main.cpp
 OBJS:=hello.o main.o
 TARGET:=hello.exe
 
-## 定义obj文件的编译规则
-hello.o : hello.cpp hello.h
-	$(CXX) $(CXXFLAGS) -c $< -o $@
-main.o : main.cpp hello.h
-	$(CXX) $(CXXFLAGS) -c $< -o $@
+## -MMD 可以用于自动生成头文件依赖关系
+%.o : %.cpp
+	$(CXX) $(CXXFLAGS) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -MT"$(@:%.o=%.d)" -c "$<" -o "$@" 
+
+DEPS := hello.d main.d
+ifneq "$(MAKECMDGOALS)" "clean"
+-include $(DEPS)
+endif
 	
 ## 定义可执行文件hello.exe的编译规则	
 $(TARGET):	$(OBJS)
@@ -37,4 +42,4 @@ all:	$(TARGET)
 ## 定义 目标clean，一般就是删除所有obj文件，以及可执行文件。
 .PHONY : clean
 clean:
-	$(RM) $(TARGET) $(OBJS)
+	$(RM) $(TARGET) $(OBJS) $(DEPS)
