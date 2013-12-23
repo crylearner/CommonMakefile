5256423b4ae7f26c5f10ed0c6a31105ade606629
yaoyufei <yaoyufei@yfyao.com>
2013-12-23 21:00:00 +0800

    完善平台编译变量

--- build/platforms/platform.mk
+++ build/platforms/platform.mk
@@ -4,10 +4,22 @@
 #     Author: lenovo
 
 ## 定义编译命
-CXX:=g++
-CXXFLAGS:=-Wall
-AR:=ar
-ARFLAGS:=-rv
+PLATFORM_NAME := x86-linux
+
+AR := ar
+ARFLAGS := -rv
+
+CC := gcc
+CPP := gcc -E
+CXX := g++
+CPPFLAGS := 
+CFLAGS := -Wall
+CXXFLAGS := -Wall
+
+LD := $(CXX)
+LDLIBS :=
+LDFLAGS :=
+
 
 ## 加载额外定制的编译命令配置.mk
 include build/platforms/platform-specs.mk
