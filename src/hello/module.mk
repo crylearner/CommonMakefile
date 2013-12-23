0beffbdbcc34232ca6f90280490f1b6b770821e4
yaoyufei <yaoyufei@yfyao.com>
2013-12-23 21:33:28 +0800

    引入多模块

--- src/hello/module.mk
+++ src/hello/module.mk
@@ -6,7 +6,7 @@
 include build/systems/system.mk
 
 ## 定义产品信息。这里BUILD_SPECS设置为spec.mk
-PRODUCT_SPECS :=
+PRODUCT_SPECS :=src/hello/spec.mk
 include build/products/product.mk
 
 ## 定义平台相关的编译命令。这里PLATFORM_SPECS为空，表示我们不做额外定制化。
@@ -18,9 +18,8 @@ CXXFLAGS+=$(OPTIONS)
 
 ## 只要给定源文件目录以及目标hello.exe
 ## 调用executable.mk，就可以自动编译得到想要的可执行文件
-SRC_FILES:= src/main.cpp
-LDLIBS += src/hello/hello.a
-TARGET:=hello.exe
+SRC_FILES:= src/hello/hello.cpp
+TARGET:= src/hello/hello.a
 
-## 定义了如何生成可执行文件的通用规则
-include build/targets/executable.mk
+## 定义了如何生成静态库的通用规则
+include build/targets/static-library.mk
