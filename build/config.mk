fa29c8e09b72d93beb340a8ad7c31f5c2e177985
sunshyran <sunshyran@gmail.com>
2014-01-09 23:18:12 +0800

    解决文件路径错误

--- build/config.mk
+++ build/config.mk
@@ -5,6 +5,7 @@
 
 DEFAULT_PLATFORM 		:= $(strip x86-linux)
 DEFAULT_PRODUCT  		:= $(strip Sample)
+DEFAULT_BUILD_TYPE		:= debug
 
 PRODUCTS_PATH			:=$(BUILD_SYSTEM_PATH)/products
 PLATFORMS_PATH			:=$(BUILD_SYSTEM_PATH)/platforms
@@ -12,7 +13,7 @@ SYSTEMS_PATH			:=$(BUILD_SYSTEM_PATH)/systems
 TARGETS_PATH			:=$(BUILD_SYSTEM_PATH)/targets
 PATHS_PATH 				:=$(BUILD_SYSTEM_PATH)/paths
 
-BUILD_EXECUTABLE 		:= $(TARGETS_PATH)/executable.mk
-BUILD_STATIC_LIBRARY 	:= $(TARGETS_PATH)/static_library.mk
-BUILD_SHARED_LIBRARY	:= $(TARGETS_PATH)/shared_library.mk
+BUILD_EXECUTABLE 		:=$(TARGETS_PATH)/executable.mk
+BUILD_STATIC_LIBRARY 	:=$(TARGETS_PATH)/static-library.mk
+BUILD_SHARED_LIBRARY	:=$(TARGETS_PATH)/shared-library.mk
 
