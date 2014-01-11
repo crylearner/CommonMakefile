ee1ceabd4d418e346878bc4715ac6eb0a5006717
sunshyran <sunshyran@gmail.com>
2014-01-11 22:02:26 +0800

    调整命名

--- build/targets/object.mk
+++ build/targets/object.mk
@@ -4,17 +4,17 @@
 #     Author: lenovo
 
 ## 从源文件列表中分别提取出C文件和C++文件
-C_OBJS  := $(patsubst %.c, $(INTERMEDIATE_OBJS_PATH)/%.o, $(filter %.c, $(SRC_FILES)))
-CPP_OBJS:= $(patsubst %.cpp, $(INTERMEDIATE_OBJS_PATH)/%.o, $(filter %.cpp, $(SRC_FILES)))
+C_OBJS  := $(patsubst %.c, $(INTERMEDIATE_OBJ_PATH)/%.o, $(filter %.c, $(SRC_FILES)))
+CPP_OBJS:= $(patsubst %.cpp, $(INTERMEDIATE_OBJ_PATH)/%.o, $(filter %.cpp, $(SRC_FILES)))
 OBJS    := $(C_OBJS) $(CPP_OBJS)
 DEPS    := $(OBJS:.o=.d)
 
 
 ## -MMD 可以用于自动生成头文件依赖关系
-$(INTERMEDIATE_OBJS_PATH)/%.o : %.cpp
+$(INTERMEDIATE_OBJ_PATH)/%.o : %.cpp
 	$(hide)$(MAKEDIR) $(dir $@)
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -MT"$(@:%.o=%.d)" -c "$<" -o "$@" 
-$(INTERMEDIATE_OBJS_PATH)/%.o : %.c
+$(INTERMEDIATE_OBJ_PATH)/%.o : %.c
 	$(hide)$(MAKEDIR) $(dir $@)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -MT"$(@:%.o=%.d)" -c "$<" -o "$@" 
 
