e735f5cd16d8567b6617ea1c411bb8bda6cc4237
sunshyran <sunshyran@gmail.com>
2014-01-12 20:19:39 +0800

    Spec：增加模板文件夹，方便用户创建module.mk

---
+++ build/templates/module.mk
@@ -0,0 +1,23 @@
+# module.mk
+#
+# Created on: 2013-12-21
+#     Author: lenovo
+
+MODULE_PATH			:= $(call current_path)
+MODULE_NAME			:= #TODO
+
+## 定义product定制文件
+PRODUCT_SPECS 		:= #TODO
+## 定义platform定制文件
+PLATFORM_SPECS 		:= #TODO
+## 定义target定制文件
+TARGET_SPECS 		:= #TODO
+
+## 生成产品、平台相关的信息，在此之前不要使用相关的变量，如CXX， CPPFLAGS，会直接覆盖。
+include $(BUILD_CONFIGURE)
+
+## 定义源文件列表
+SRC_FILES:= $(MODULE_PATH)/main.cpp
+
+## 调用目标编译规则
+$(call build_target,$(BUILD_EXECUTABLE))
